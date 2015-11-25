0786591721621f26f8cf3478b109fe3b69911721
s-a-s-h-a <olexandr770@gmail.com>
2015-11-25 12:32:40 +0200

    Main page, some block

---
+++ js/app.js
@@ -0,0 +1,65 @@
+
+// js main heder superslides
+$(document).ready(function() {
+  $(document).on('init.slides', function() {
+    $('.loading-container').fadeOut(function() {
+      $(this).remove();
+    });
+  });
+
+  $('#slides').superslides({
+    slide_easing: 'easeInOutCubic',
+    slide_speed: 800,
+    pagination: true,
+    hashchange: false,
+    play: 5000,
+    scrollable: true
+  });
+
+  document.ontouchmove = function(e) {
+    e.preventDefault();
+  };
+  $('#slides').hammer().on('swipeleft', function() {
+    $(this).superslides('animate', 'next');
+  });
+
+  $('#slides').hammer().on('swiperight', function() {
+    $(this).superslides('animate', 'prev');
+  });
+
+});
+//And js main heder superslides
+//Alignment unit .we_height Main page
+$(function() {
+    $('.we_height').matchHeight();
+});
+//And Alignment unit  .we_height Main page
+
+
+// Sidebar Navigation  Main page
+    $(document).ready(function () {
+
+
+        $('.scroll-container').onePageScroll({
+            topElement: $('header'),
+//            position: 'left',
+//            background: 'rgba(50,255,100, 0.9)',
+//            tooltip: false,
+//            tooltipSelector: $('h3'),
+//            emptyContent: true,
+            sectionSelector: $('.content-part')
+        });
+
+        $('.setOff').click(function () {
+            console.log('off');
+            $('.scroll-container').onePageScrollDestroy();
+        })
+    });
+//And Sidebar Navigation  Main page
+
+
+//Alignment unit .choos_content Main page
+$(function() {
+    $('.choos_content').matchHeight();
+});
+//And Alignment unit  .we_height Main page
