ecabcde6c3b3b5574ebd433df43e45539ecc2cce
s-a-s-h-a <olexandr770@gmail.com>
2015-12-01 16:50:35 +0200

    Fixed animate, texts

--- js/app.js
+++ js/app.js
@@ -149,20 +149,3 @@ $(document).ready(function() {
 //And Form animated Main page
 
 
-
-    $(document).ready(function(){ 
- 
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 1200) {
-                $('.scrollup').fadeIn();
-            } else {
-                $('.scrollup').fadeOut();
-            }
-        }); 
- 
-        $('.scrollup').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 1000);
-            return false;
-        });
- 
-    });
