125e0ba39b5205bc206688277ab81465745a8751
s-a-s-h-a <olexandr770@gmail.com>
2015-12-05 21:14:49 +0200

    Services page

--- js/app.js
+++ js/app.js
@@ -1,37 +1,44 @@
-
-// js main heder superslides
-$(document).ready(function() {
-  $(document).on('init.slides', function() {
-    $('.loading-container').fadeOut(function() {
-      $(this).remove();
-    });
+//Slider content services
+$( document ).ready(function( $ ) {
+    $( '#example5' ).sliderPro({
+        width: 830,
+        height: 500,
+        autoplay: false,
+        orientation: 'horizontal',
+        slideDistance: 0,
+        loop: false,
+        fade: true,
+        arrows: true,
+        buttons: false,
+        thumbnailsPosition: 'left',
+        thumbnailPointer: true,
+        thumbnailWidth: 350,
+        thumbnailHeight: 71.8,
+        breakpoints: {
+          800: {
+            thumbnailsPosition: 'top',
+            thumbnailWidth: 350,
+            thumbnailHeight: 71.8
+          },
+          500: {
+            thumbnailsPosition: 'top',
+            thumbnailWidth: 120,
+            thumbnailHeight: 50
+          }
+        }
+      });
   });
 
-  $('#slides').superslides({
-    slide_easing: 'easeInOutCubic',
-    slide_speed: 800,
-    pagination: true,
-    animation: 'fade',
-    hashchange: false,
-    play: 5000,
-    scrollable: true
-  });
 
 
-  $('#slides').hammer().on('swipeleft', function() {
-    $(this).superslides('animate', 'next');
-  });
 
-  $('#slides').hammer().on('swiperight', function() {
-    $(this).superslides('animate', 'prev');
-  });
 
-});
-//And js main heder superslides
 //Alignment unit .we_height Main page
+    $(document).ready(function () {
 $(function() {
     $('.we_height').matchHeight();
 });
+    });
 //And Alignment unit  .we_height Main page
 
 
@@ -61,91 +68,14 @@ $(function() {
 //And Sidebar Navigation  Main page
 
 
-//Alignment unit .choos_content Main page
-$(function() {
-    $('.choos_content').matchHeight();
-});
-//And Alignment unit  .we_height Main page
-
-//Alignment unit .choos_content Main page
-$(function() {
-    $('.tt_bot').matchHeight();
-});
-//And Alignment unit  .if_height Main page
-
-//Alignment unit .choos_content Map Main page
-$(function() {
-    $('.footr_form').matchHeight();
-});
-//And Alignment unit  .if_height Map Main page
-
-// Slider Main page
-$(document).ready(function() {
- 
-  $("#reviews").owlCarousel({
-    pagination: false,
-    navigation: true,
-    singleItem : true
-  });
- 
-});
-//End Slider Main page
 
 
-// Slider Main page
-$(document).ready(function(){
-  $('.industrieswehelp').bxSlider({
-    pagerSelector: '.industriewwepager',
-    auto: true
-  });
-});
-//End Slider Main page
-// Animated css scroll
-$(document).ready(function(){
-new WOW().init();
-});
-// Animated css scroll
-// Form animated Main page
-$(document).ready(function() {
-
-  $('input').each(function() {
-
-    $(this).on('focus', function() {
-      $(this).parent('.css').addClass('active');
-    });
-
-    $(this).on('blur', function() {
-      if ($(this).val().length == 0) {
-        $(this).parent('.css').removeClass('active');
-      }
-    });
-
-    if ($(this).val() != '') $(this).parent('.css').addClass('active');
-
-  });
-
-});
 
 
-$(document).ready(function() {
 
-  $('textarea').each(function() {
 
-    $(this).on('focus', function() {
-      $(this).parent('.css').addClass('active');
-    });
 
-    $(this).on('blur', function() {
-      if ($(this).val().length == 0) {
-        $(this).parent('.css').removeClass('active');
-      }
-    });
 
-    if ($(this).val() != '') $(this).parent('.css').addClass('active');
 
-  });
-
-});
-//And Form animated Main page
 
 
