486d36f920bf9586e4c13d86e86bd57f893ed443
s-a-s-h-a <olexandr770@gmail.com>
2015-12-08 18:26:44 +0200

    About us

---
+++ js/about.js
@@ -0,0 +1,17 @@
+
+  //Alignment unit .we_height Main page
+    $(document).ready(function () {
+$(function() {
+    $('.team_i_heig').matchHeight();
+});
+    });
+//And Alignment unit  .we_height Main page
+
+
+  //Alignment unit .we_height Main page
+    $(document).ready(function () {
+$(function() {
+    $('.team_initials').matchHeight();
+});
+    });
+//And Alignment unit  .we_height Main page
