813aa174b9b052f0a5f906ae1d035f71274a5c7e
s-a-s-h-a <olexandr770@gmail.com>
2015-12-14 18:02:57 +0200

    Benefits

--- js/benefits.js
+++ js/benefits.js
@@ -1,16 +1,13 @@
 
   //Benefits page accordeon plagin uses for work libs jquery, jquery easing, jquery.accordion.js
             $(function() {
-			
 				$('#benefits-accordion').accordion({
 					oneOpenedItem	: true,
 					easing          : 'linear',
-					scrollSpeed     : 800,
-					scrollEasing    : 'linear',
-					speed           : 0
-
-				});
-				
+					speed           : 0,
+					scrollSpeed     : 400,
+					scrollEasing    : 'linear'
+				});	
             });
 
             
